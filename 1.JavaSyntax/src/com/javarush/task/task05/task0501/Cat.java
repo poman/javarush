733d98bbeb2e9cf048459953a84e4f874cf9c896
Roman Kukla <kukla.roman@gmail.com>
2017-03-19 19:32:07 +0200

    501 task

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.name = "Vaska";
+        cat.age = 1;
+        cat.weight = 3;
+        cat.strength = 5;
+    }
+
+
+}
