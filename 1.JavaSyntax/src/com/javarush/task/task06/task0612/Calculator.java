2783f503e2a7fe54060efb1b89c8f4fc6aba657e
Roman Kukla <kukla.roman@gmail.com>
2017-03-22 22:47:59 +0200

    612 task

--- 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
@@ -6,28 +6,24 @@ package com.javarush.task.task06.task0612;
 
 public class Calculator {
     public static int plus(int a, int b) {
-        //напишите тут ваш код
-        return 0;
+        return a + b;
     }
 
     public static int minus(int a, int b) {
-        //напишите тут ваш код
-        return 0;
+        return a - b;
     }
 
     public static int multiply(int a, int b) {
-        //напишите тут ваш код
-        return 0;
+        return a*b;
     }
 
     public static double division(int a, int b) {
-        //напишите тут ваш код
-        return 0;
+        return (double) a / b;
     }
 
     public static double percent(int a, int b) {
-        //напишите тут ваш код
-        return 0;
+        double prc = (double)a/100D;
+        return prc * b;
     }
 
     public static void main(String[] args) {
