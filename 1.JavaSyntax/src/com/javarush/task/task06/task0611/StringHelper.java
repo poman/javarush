53ff6db6980b1237367b9be70580d3488d788f5d
Roman Kukla <kukla.roman@gmail.com>
2017-03-22 18:29:43 +0200

    611 task

--- 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
@@ -7,13 +7,19 @@ package com.javarush.task.task06.task0611;
 public class StringHelper {
     public static String multiply(String s) {
         String result = "";
-        //напишите тут ваш код
+        for (int i = 0; i < 5 ; i++) {
+            result +=s;
+            System.out.print(s);
+        }
         return result;
     }
 
     public static String multiply(String s, int count) {
         String result = "";
-        //напишите тут ваш код
+        for (int i = 0; i < count; i++) {
+            result +=s;
+            System.out.print(s);
+        }
         return result;
     }
 
