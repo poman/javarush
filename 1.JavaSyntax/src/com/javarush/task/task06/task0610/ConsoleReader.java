61d4ab54e23158dc78c11e5ef8e3049af7ae689e
Roman Kukla <kukla.roman@gmail.com>
2017-03-22 17:53:43 +0200

    610 task

--- 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -9,23 +9,30 @@ import java.io.InputStreamReader;
 
 public class ConsoleReader {
     public static String readString() throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        return str;
 
     }
 
     public static int readInt() throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int chislo = Integer.decode(reader.readLine());
+        return chislo;
 
     }
 
     public static double readDouble() throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double result = Double.parseDouble(reader.readLine());
+        return result;
 
     }
 
     public static boolean readBoolean() throws Exception {
-        //напишите тут ваш код
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean res = Boolean.parseBoolean(reader.readLine());
+        return res;
     }
 
     public static void main(String[] args) {
