2edadf28b46cfa0cac0b7f718ad20d1b10926ae0
Roman Kukla <kukla.roman@gmail.com>
2017-03-23 20:59:40 +0200

    614-615 tasks

--- 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
@@ -7,17 +7,25 @@ import java.util.ArrayList;
 */
 
 public class Cat {
-    //напишите тут ваш код
+    public static ArrayList cats = new ArrayList();
 
     public Cat() {
     }
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < 10; i++) {
+            Cat cat = new Cat();
+            cats.add(cat);
+        }
         printCats();
+
     }
 
     public static void printCats() {
-        //напишите тут ваш код
+        for (int i = 0; i < cats.size(); i++) {
+            System.out.println(cats.get(i));
+        }
+
+
     }
 }
