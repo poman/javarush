4c2074d1719fe163c289553d2302aa53c6a10dbd
Roman Kukla <kukla.roman@gmail.com>
2017-03-24 17:49:14 +0200

    705 task

--- 1.JavaSyntax/src/com/javarush/task/task07/task0705/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0705/Solution.java
@@ -9,6 +9,26 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        int[] listInt = new int[20];
+        int[] list1 = new int[10];
+        int[] list2 = new int[10];
+        int j = 0;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < listInt.length; i++) {
+            int s = Integer.decode(reader.readLine());
+            listInt[i] = s;
+        }
+
+        for (int i = 0; i < list1.length ; i++) {
+            list1[i] = listInt[i];
+        }
+        for (int i = 10; i < listInt.length; i++) {
+            list2[j] = listInt[i];
+            j++;
+        }
+
+        for (int i = 0; i < list2.length; i++) {
+            System.out.println(list2[i]);
+        }
     }
 }
