6a70475a4b159987afc06bddf419d7d38938bf70
Roman Kukla <kukla.roman@gmail.com>
2017-03-24 10:59:22 +0200

    618 task

--- 1.JavaSyntax/src/com/javarush/task/task06/task0618/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0618/Solution.java
@@ -5,12 +5,12 @@ KissMyShinyMetalAss
 */
 
 public class Solution {
-    public static class ??? {
+    public static class KissMyShinyMetalAss {
 
     }
 
     public static void main(String[] args) {
-        //напишите тут ваш код
-        System.out.println(???);
+        KissMyShinyMetalAss kissMyShinyMetalAss = new KissMyShinyMetalAss();
+        System.out.println(kissMyShinyMetalAss);
     }
 }
