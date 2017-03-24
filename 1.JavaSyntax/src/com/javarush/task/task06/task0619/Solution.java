25028af3f08c9ea9147663a2d628a0ffb6c0aa16
Roman Kukla <kukla.roman@gmail.com>
2017-03-24 11:57:40 +0200

    619 task

--- 1.JavaSyntax/src/com/javarush/task/task06/task0619/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0619/Solution.java
@@ -5,10 +5,15 @@ package com.javarush.task.task06.task0619;
 */
 
 public class Solution {
+
+    public static String name = "solution text";
+
     public static class Cat {
+        public static String name = "cat name here";
     }
 
     public static class Dog {
+        public static String name = "dog name here";
     }
 
     public static void main(String[] args) {
