df573e9c69ae731534cd50fb8502d4eb5268d1b2
Roman Kukla <kukla.roman@gmail.com>
2017-03-28 11:24:13 +0300

    718 task

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Проверка на упорядоченность
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (i > 0 && (list.get(i-1).length() > list.get(i).length())){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
+
