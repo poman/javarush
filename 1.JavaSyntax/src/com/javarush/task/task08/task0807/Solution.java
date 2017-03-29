3346efd79a056358e6a9e523cff80e2e9282e94f
Roman Kukla <kukla.roman@gmail.com>
2017-03-29 17:31:02 +0300

    807 task

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        ArrayList<String> list = new ArrayList<String>();
+        return list;
+    }
+
+    public static Object createLinkedList() {
+        LinkedList<String> linkedList = new LinkedList<String>();
+        return linkedList;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
