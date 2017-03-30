6ca0031683096263ccf220e9e43c3a363284b194
Roman Kukla <kukla.roman@gmail.com>
2017-03-30 10:33:24 +0300

    811 task

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Квартет «Методы»
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        ArrayList<String> list = new ArrayList<String>();
+        return list;
+
+    }
+
+    public static List getListForSet() {
+        ArrayList<String> list = new ArrayList<String>();
+        return list;
+
+    }
+
+    public static List getListForAddOrInsert() {
+        LinkedList<String> list = new LinkedList<String>();
+        return list;
+
+    }
+
+    public static List getListForRemove() {
+        LinkedList<String> list = new LinkedList<String>();
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
